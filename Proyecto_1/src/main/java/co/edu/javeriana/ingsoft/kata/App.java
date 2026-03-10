64ff4f81a8118a7ad07b553f35ce66761523ddef
Samuel López <samuellopez@javeriana.edu.co>
2026-03-09 20:20:41 -0500

    Completado: Lógica FizzBuzz, pruebas unitarias y clase App para impresión

--- Proyecto_1/src/main/java/co/edu/javeriana/ingsoft/kata/App.java
+++ Proyecto_1/src/main/java/co/edu/javeriana/ingsoft/kata/App.java
@@ -1,13 +1,16 @@
-package co.edu.javeriana.ingsoft.kata;
+package co.edu.javeriana.ingsoft.kata.FizzBuzz;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        System.out.println("--- Iniciando FizzBuzz del 1 al 100 ---");
+
+        for (int i = 1; i <= 100; i++) {
+            String result = fizzBuzz.calculate(i);
+            System.out.println(i + ": " + result);
+        }
+
+        System.out.println("--- Fin del programa ---");
     }
 }
